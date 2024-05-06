a57514009b007eff2306d36b31bda353457c1d62
Sime Roncevic <sroncevic19@gmail.com>
2024-05-06 17:09:01 +0200

    Removing few files

--- CareConnectReactApp/src/App.jsx
+++ CareConnectReactApp/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css'
-import ChooseAccountComponent from './LogingComponents/ChooseAcountComponents/ChooseAccountComponent';
+
 
 function App() {
   
@@ -10,7 +10,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<ChooseAccountComponent></ChooseAccountComponent>} />
+          
         </Routes>
       </Router>
     </>
