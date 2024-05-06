1e72ef7918bc5f97e14b847d1be3b583b42477ba
Sime Roncevic <sroncevic19@gmail.com>
2024-05-06 16:42:19 +0200

    Creating login screen with remebered accounts

--- CareConnectReactApp/src/App.jsx
+++ CareConnectReactApp/src/App.jsx
@@ -1,12 +1,19 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css'
+import ChooseAccountComponent from './LogingComponents/ChooseAcountComponents/ChooseAccountComponent';
 
 function App() {
   
 
   return (
-    <></>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<ChooseAccountComponent></ChooseAccountComponent>} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
---
+++ CareConnectReactApp/src/RemeberedProfilesData.js
@@ -0,0 +1,16 @@
+import profilePic1 from './assets/logo.jpg'; 
+
+const remeberdProfilesData = [
+    {
+        id: 1,
+        img: profilePic1,
+        nameSurname: "Šime Rončević",
+    },
+    {
+        id: 2,
+        img: profilePic1,
+        nameSurname: "Josip Rončević",
+    }
+];
+
+export default remeberdProfilesData
