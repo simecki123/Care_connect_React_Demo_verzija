8455c1bd26262c8dd55cc1546298278ce9365876
Sime Roncevic <sroncevic19@gmail.com>
2024-05-12 11:15:16 +0200

    Creating YourDoctorProfileComponent

--- CareConnectReactApp/src/YourDoctorComponents/YourDoctorComponent.jsx
+++ CareConnectReactApp/src/YourDoctorComponents/YourDoctorComponent.jsx
@@ -1,11 +1,13 @@
 import HeaderComponent from "../MainPageComponents/HeaderComponents/HeaderComponent";
+import YourDoctorProfileComponent from "./YourDoctorProfileComponent/YourDoctorProfileComponent";
 
 
 function YourDoctorComponent() {
     return(
         <>
             <HeaderComponent />
-            
+            <YourDoctorProfileComponent />
+            <YourDoctorProfileComponent />
         </>
     );
 }
--- CareConnectReactApp/src/YourDoctorComponents/YourDoctorProfileComponent/YourDoctorProfileComponent.jsx
+++ CareConnectReactApp/src/YourDoctorComponents/YourDoctorProfileComponent/YourDoctorProfileComponent.jsx
@@ -1,1 +1,29 @@
+import './YourDoctorProfileStyle.css';
+import logo from '../../assets/logo.jpg';
 
+
+function YourDoctorProfileComponent() {
+    return(
+        <>
+            <div className="doctor-profileContainer-big">
+                <div className='doctor-profile-pic-container'>
+                    <img className='doctor-profile-pic' src={logo} />
+                </div>
+
+                <div className='name-surname-container'>
+                    <label className='doctor-name-surname-label'>Skibidi toilet</label>
+                </div>
+
+                <div className='title-of-doctor-container'>
+                    <label className='title-of-doctor'>dr.sc</label>
+                </div>
+
+                <div className='doctor-profile-description'>
+                    <p className='doctor-profile-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque id aperiam iusto unde perferendis autem, quia illo, quod illum delectus quae blanditiis dolore. Et facere alias dignissimos eveniet sapiente reprehenderit!</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default YourDoctorProfileComponent;
