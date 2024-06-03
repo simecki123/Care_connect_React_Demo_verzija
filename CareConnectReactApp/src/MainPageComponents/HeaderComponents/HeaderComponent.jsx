65fc0fff5f5ea3a908be4ee4fb5c9162fd680de4
Sime Roncevic <sroncevic19@gmail.com>
2024-06-03 19:40:08 +0200

    Adding view for smartphones

--- CareConnectReactApp/src/MainPageComponents/HeaderComponents/HeaderComponent.jsx
+++ CareConnectReactApp/src/MainPageComponents/HeaderComponents/HeaderComponent.jsx
@@ -1,64 +1,71 @@
+import React, { useState } from 'react';
 import './HeaderStyle.css';
 import logo from '../../assets/logo.jpg';
 import { Link } from 'react-router-dom';
 
 function HeaderComponent() {
-    return(
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
         <>
             <header>
-
                 <div className='header-container'>
                     <Link className='options-container' to={"/mainpage"}>
                         <div className='logo-header-container'>
-                            <img className='header-logo' src={logo}></img>
+                            <img className='header-logo' src={logo} alt='Logo' />
                         </div>
                     </Link>
 
-                    
-
-                    <Link className='options-container' to={"/devices"}>
-                        <div className='options-container'>
-                                <label className='option-label'>Devices</label>
-                        </div>
-                    </Link>
-
-                    <Link className='options-container' to={"/yourDoctor"}>
-                        <div className='options-container'>
-                            <label className='option-label'>Your Doctor</label>
-                        </div>
-                    </Link>
-
-                    <Link className='options-container' to={"/convoHistory"}>
-                        <div className='options-container'>
-                            <label className='option-label'>Convo History</label>
-                        </div>
-                    </Link>
-
-                    <Link className='options-container' to={"/medicalHistory"}>
-                        <div className='options-container'>
-                            <label className='option-label'>Medical history</label>
-                        </div>
-                    </Link>
-
-                    <Link className='options-container' to={"/medicalDocuments"}>
-                    <div className='options-container'>
-                        <label className='option-label'>Medical Documents</label>
+                    <div className={`menu-icon ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                        <div className='hamburger'></div>
                     </div>
-                    </Link>
 
-                    <Link className='options-container' to={"/medications"}>
-                        <div className='options-container'>
-                            <label className='option-label'>Medications</label>
-                        </div>
-                    </Link>
+                    <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+                        <Link className='options-container' to={"/devices"}>
+                            <div className='options-container'>
+                                <label className='option-label'>Devices</label>
+                            </div>
+                        </Link>
 
+                        <Link className='options-container' to={"/yourDoctor"}>
+                            <div className='options-container'>
+                                <label className='option-label'>Your Doctor</label>
+                            </div>
+                        </Link>
+
+                        <Link className='options-container' to={"/convoHistory"}>
+                            <div className='options-container'>
+                                <label className='option-label'>Convo History</label>
+                            </div>
+                        </Link>
+
+                        <Link className='options-container' to={"/medicalHistory"}>
+                            <div className='options-container'>
+                                <label className='option-label'>Medical history</label>
+                            </div>
+                        </Link>
+
+                        <Link className='options-container' to={"/medicalDocuments"}>
+                            <div className='options-container'>
+                                <label className='option-label'>Medical Documents</label>
+                            </div>
+                        </Link>
+
+                        <Link className='options-container' to={"/medications"}>
+                            <div className='options-container'>
+                                <label className='option-label'>Medications</label>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
                 <hr className='header-break-line' />
-
             </header>
         </>
     );
 }
-
 
 export default HeaderComponent;
